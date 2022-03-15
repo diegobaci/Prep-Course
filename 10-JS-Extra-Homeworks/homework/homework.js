90b84c5ce2dc5853ef4c607fbfedd568c299a33d
Diego Bacigalupo <diegobaci19@outlook.es>
2022-03-15 00:53:35 -0300

    finish homework 1

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -17,7 +17,7 @@ function numberOfCharacters(string) {
   //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
-  //Escribe tu código aquí
+  //"aabbcccddeeefffggghh"- { a: 2, b: 2, c: 3, d: 2, e: 3, f: 3, g: 3, h: 2 }
 }
 
 
